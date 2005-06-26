8f360d56ae4aede030127438b0bd4c68ad2b7fbf
dsd <dsd@00c4350b-4cf9-0310-8c1b-f3fed11d2be3>
2005-06-26 14:52:50 +0000

    Add website scripts
    git-svn-id: svn://anonsvn.gentoo.org/linux-patches@55 00c4350b-4cf9-0310-8c1b-f3fed11d2be3

---
+++ genpatches-2.6-misc/web/gentoo_sources_web.pm
@@ -0,0 +1,272 @@
+# Copyright 2000-2005 Gentoo Foundation; Distributed under the GPL v2
+
+$genpatches_path = '/home/dsd/projects/gentoo/genpatches';
+$subversion_root = 'svn+ssh://dsd@svn.gentoo.org/var/svnroot/linux-patches/genpatches-2.6';
+$webscript_path = '/home/dsd/projects/gentoo/genpatches-misc/web';
+$output_path = $webscript_path.'/output';
+
+$website_base = 'http://dev.gentoo.org/~dsd/genpatches';
+
+$ebuild_base = '/usr/local/gentoo-x86';
+@kernels = ('sys-kernel/gentoo-sources', 'sys-kernel/ck-sources');
+
+sub html_header {
+	local *FD = shift;
+	my $title = shift;
+	$title = 'genpatches-2.6 infopage' if !$title;
+	
+	print FD '<html>';
+	print FD '<head>';
+	print FD '<title>'.$title.'</title>';
+	print FD '<link rel="stylesheet" href="style.css" />';
+	print FD '</head>';
+	print FD '<body>';
+}
+
+sub html_footer {
+	local *FD = shift;
+	my $date = `date`;
+	my $user = `whoami`;
+
+	print FD '<hr /><div align="right">Automatically generated: '.$date.' by '.$user.'</div>';
+	print FD '</body>';
+	print FD '</html>';
+}
+
+# From CGI::MxScreen
+sub html_escape {
+	my $t = shift;
+	$t =~ s/&/&amp;/g;
+	$t =~ s/\"/&quot;/g;
+	$t =~ s/>/&gt;/g;
+	$t =~ s/</&lt;/g;
+	return $t;
+}
+
+# Shamelessly stolen from irc2html
+# irc2html.pl --- converts raw IRC logs to a readable HTML table-ized form.
+# Copyright © 1998, 1999, 2000, 2002, 2003, 2004 Jamie Zawinski <jwz@jwz.org>
+sub html_urlify {
+	my $str = shift;
+	my $url_re = q{\b(s?https?|ftp|file|gopher|s?news|telnet|mailbox):} .
+		q{(//[-A-Z0-9_.]+:\d*)?} .
+		q{[-A-Z0-9_=?\#\$\@~\`%&*+|\/.,;\240]+};
+	$str =~ s@($url_re)@<A HREF="$1">$1</A>@gi;
+	return $str;
+}
+
+sub issues_get_questions {
+	my $file = shift;
+	my $out;
+	local *ISSUES;
+	
+	open(ISSUES, '< '.$file);
+	foreach (<ISSUES>) {
+		chomp;
+		last if $_ eq '--';
+		$out .= $_ . "\n";
+	}
+	close(ISSUES);
+	return $out;
+}
+
+sub issues_get_answers {
+	my $file = shift;
+	my ($out, $ddcount);
+	local *ISSUES;
+	$ddcount = 0;
+	
+	open(ISSUES, '< '.$file);
+	foreach (<ISSUES>) {
+		chomp;
+		if ($_ eq '--') {
+			$ddcount++;
+			next;
+		}
+		$out .= $_ . "\n" if $ddcount == 1;
+	}
+	close(ISSUES);
+	return $out;
+}
+
+sub issues_get_info {
+	my $file = shift;
+	my ($out, $ddcount);
+	local *ISSUES;
+	$ddcount = 0;
+	
+	open(ISSUES, '< '.$file);
+	foreach (<ISSUES>) {
+		chomp;
+		if ($_ eq '--') {
+			$ddcount++;
+			next;
+		}
+		if ($ddcount == 2) {
+			if ($_ =~ /^#([0-9]+)$/) {
+				$out .= '<a href="http://bugs.gentoo.org/'.$1.'">Bug '.$1.'</a>' . "\n";
+			} elsif ($_ =~ /^[0-9]{8}/) {
+				$out .= "Date: $_\n";
+			} elsif ($_ =~ /^[0-9]\.[0-9]\.[0-9]$/) {
+				$out .= "Kernel: $_\n";
+			}
+		}
+	}
+	close(ISSUES);
+	return $out;
+}
+
+sub nl2br {
+	my $content = shift;
+	$content =~ s/\n/<br \/>/g;
+	return $content;
+}
+
+sub include_content {
+	local *FD = shift;
+	my $page = shift;
+	local *CONTENT;
+
+	open(CONTENT, '< '.$webscript_path.'/content/'.$page.'.htm');
+	print FD $_ foreach <CONTENT>;
+	close(CONTENT);
+}
+
+sub include_generated {
+	local *FD = shift;
+	my $page = shift;
+	local *CONTENT;
+
+	open(CONTENT, '< '.$webscript_path.'/generated/'.$page);
+	print FD $_ foreach <CONTENT>;
+	close(CONTENT);
+}
+
+sub include_faq {
+	local *FD = shift; 
+	my $page = shift;
+	local *CONTENT;
+	my $i;
+	
+	open(CONTENT, '< '.$webscript_path.'/content/faq/'.$page);
+	$i = 0;
+	foreach (<CONTENT>) {
+		if ($i++ == 0) {
+			print FD '<b>Q. '.$_.'</b><br />';
+		} else {
+			print FD $_ . '<br />';
+		}
+	}
+	close(CONTENT);
+	print FD '<br /><br />';
+}
+
+sub _get_patch_list {
+	my $tag = shift;
+	my $cmd = 'svn cat '.$subversion_root.'/tags/'.$tag.'/0000_README';
+	my @readme_lines = `$cmd`;
+	my @patches;
+	my $count = -1;
+
+	foreach (@readme_lines) {
+		chomp;
+		
+		if (/^[Pp]atch:\t(.*)$/) {
+			$count++;
+			$patches[$count]{'patch'} = $1;
+		}
+
+		if (/^[Ff]rom:\t(.*)$/) {
+			$patches[$count]{'from'} = $1;
+		}
+
+		if (/^[Dd]esc:\t(.*)$/) {
+			$patches[$count]{'desc'} = $1;
+		}
+	}
+
+	return @patches;
+}
+
+sub _parse_log {
+	my $tag = shift;
+	my $lastrev = shift;
+	my (@commits, $state, $rev);
+	my $cmd = 'svn log -v -r '.$lastrev.':HEAD '.$subversion_root.'/tags/'.$tag;
+	my @loglines = `$cmd`;
+
+	foreach (@loglines) {
+		if (/^-+$/) {
+			$state = '';
+			next;
+		}
+
+		if ($state eq 'wantpaths') {
+			if (/^\s+([A-Z]) \/genpatches-2\.6\/trunk\/[\d\.]+\/([^\s]+)/) {
+				push (@{$commits[$rev]{"action$1"}}, $2) if $2 != "0000_README";
+			} elsif (/^$/) {
+				$state = 'wantlog';
+			}
+			next;
+		}
+
+		if ($state eq 'wantlog') {
+			$commits[$rev]{'logmsg'} .= "\n$_";
+			next;
+		}
+
+		if (/^r(\d+) \| (\w+) \|/) {
+			$state = 'wantpaths';
+			$rev = $1;
+			$commits[$rev]{'rev'} = $rev;
+			$commits[$rev]{'author'} = $2;
+			next;
+		}
+	}
+
+	return @commits;
+}
+
+sub release_is_generated {
+	my $tag = shift;
+	return -e $webscript_path.'/generated/'.$tag.'-patches.htm' &&
+		-e $webscript_path.'/generated/'.$tag.'-info.htm';
+}
+
+sub _get_genpatches_kernels {
+	my (%gp_kernels, $kernel);
+
+	foreach $kernel (@kernels) {
+		$kernel =~ m/^([a-z-]+)\/([a-z-]+)$/;
+		my $cat = $1;
+		my $pkg = $2;
+		$cmd = 'egrep "^(K_GENPATCHES_VER|K_WANT_GENPATCHES)" '.$ebuild_base.'/'.$kernel.'/*.ebuild';
+		my @out = `$cmd`;
+
+		foreach (@out) {
+			chomp;
+			my $res = substr($_, length($ebuild_base) + length($kernel) + 2);
+			$res =~ m/^$pkg-([\d\w\.-]+)\.ebuild:(.*)$/;
+			my $ver = $1;
+			my $var = $2;
+			my $ebuild = $pkg.'-'.$ver;
+			$ver =~ m/^(2\.6\.\d+)/;
+			my $orig_ver = $1;
+	
+			if ($var =~ /^K_WANT_GENPATCHES="(.*)"$/) {
+				$gp_kernels{$ebuild}{'pkg'} = $pkg;
+				$gp_kernels{$ebuild}{'ver'} = $ver;
+				$gp_kernels{$ebuild}{'wanted'} = $1;
+			}
+			if ($var =~ /^K_GENPATCHES_VER="(\d+)"$/) {
+				$gp_kernels{$ebuild}{'gprev'} = $orig_ver .'-'. $1;
+			}
+		}
+	}
+
+	return %gp_kernels;
+}
+
+
+1;
+
---
+++ genpatches-2.6-misc/web/makesite.pl
@@ -0,0 +1,328 @@
+#!/usr/bin/perl
+# Copyright 2004-2005 Gentoo Foundation; Distributed under the GPL v2
+#
+# Scripts to automatically generate the gentoo-sources-2.6 information pages
+#
+# Disclaimer: This code is really ugly, I'm really good at writing horrible perl
+# But it works (to an extent) :)
+
+use File::Basename;
+use File::Copy;
+use URI::Escape;
+use gentoo_sources_web;
+
+make_index_page();
+make_about_page();
+make_faq_page();
+make_bugs_page();
+make_issues_page();
+make_kernels_page();
+make_release_pages();
+process_static_content();
+
+sub make_index_page {
+	local *FD;
+	print ">> Making index page\n";
+	open(FD, '> '.$output_path.'/index.htm');
+	html_header *FD;
+	include_content *FD, 'sitemap';
+	html_footer *FD;
+	close(FD);
+}
+
+sub make_about_page {
+	local *FD;
+	print ">> Making about page\n";
+	open(FD, '> '.$output_path.'/about.htm');
+	html_header *FD;
+	include_content *FD, 'about';
+	html_footer *FD;
+	close(FD);
+}
+
+sub make_faq_page {
+	local (*FD, *DIR);
+	print ">> Making FAQ page\n";
+	open(FD, '> '.$output_path.'/faq.htm');
+	html_header *FD;
+	print FD '<h1>Frequently Asked Questions</h1>';
+	opendir(DIR, 'content/faq');
+	include_faq *FD, $_ foreach grep { -f "content/faq/$_" } sort readdir DIR;
+	closedir(DIR);
+	html_footer *FD;
+	close(FD);
+}
+
+sub make_bugs_page {
+	local *FD;
+	print ">> Making bugs page\n";
+	open(FD, '> '.$output_path.'/bugs.htm');
+	html_header *FD;
+	include_content *FD, 'bugs';
+	html_footer *FD;
+	close(FD);
+}
+
+sub make_issues_page {
+	local *FD;
+	print ">> Making issues page\n";
+	open(FD, '> '.$output_path.'/issues.htm');
+	html_header *FD;
+	generate_issues_index(FD);
+	html_footer *FD;
+	close(FD);
+	
+	generate_issues_current();
+	generate_issues_resolved();
+}
+
+sub generate_issues_index {
+	local *FD = shift;
+	local *DIR;
+	my ($file, $path);
+
+	print FD '<h1>Current/Recent Issues and Solutions</h1>';
+	print FD '<ul>';
+	opendir(DIR, 'content/issues/current');
+	foreach $file (grep { -f "content/issues/current/$_" } sort readdir DIR) {
+		$path = 'content/issues/current/'.$file;
+		print FD '<li><a href="issues-current.htm#'.$file.'">';
+		print FD nl2br html_escape issues_get_questions $path;
+		print FD '</a></li>';
+	}
+	closedir(DIR);
+	print FD '</ul><hr />';
+	
+	print FD '<h1>Previous/Resolved Issues and Solutions</h1>';
+	print FD '<ul>';
+	opendir(DIR, 'content/issues/resolved');
+	foreach $file (grep { -f "content/issues/resolved/$_" } sort readdir DIR) {
+		$path = 'content/issues/resolved/'.$file;
+		print FD '<li><a href="issues-resolved.htm#'.$file.'">';
+		print FD nl2br html_escape issues_get_questions $path;
+		print FD '</a></li>';
+	}
+	closedir(DIR);
+	print FD '</ul>';
+}
+
+sub generate_issues_current {
+	local (*DIR, *FD);
+	my $file;
+	open(FD, '> '.$output_path.'/issues-current.htm');
+	html_header *FD;
+	print FD '<h1>Current Issues and Solutions</h1>';
+	
+	opendir(DIR, 'content/issues/current');
+	foreach $file (grep { -f "content/issues/current/$_" } sort readdir DIR) {
+		$path = 'content/issues/current/'.$file;
+		print FD '<a name="'.$file.'"></a>';
+		print FD '<b><i>';
+		print FD nl2br issues_get_questions $path;
+		print FD '</b></i><br /><b>Solution. </b>';
+		print FD nl2br issues_get_answers $path;
+		print FD '<br /><font size="2">';
+		print FD nl2br issues_get_info $path;
+		print FD '</font><br /><hr />';
+
+	}
+	closedir(DIR);
+	
+	html_footer *FD;
+	close(FD);
+}
+
+sub generate_issues_resolved {
+	local (*DIR, *FD);
+	my $file;
+	open(FD, '> '.$output_path.'/issues-resolved.htm');
+	html_header *FD;
+	print FD '<h1>Previous/Resolved Issues and Solutions</h1>';
+	
+	opendir(DIR, 'content/issues/resolved');
+	foreach $file (grep { -f "content/issues/resolved/$_" } sort readdir DIR) {
+		$path = 'content/issues/resolved/'.$file;
+		print FD '<a name="'.$file.'"></a>';
+		print FD '<b><i>';
+		print FD nl2br issues_get_questions $path;
+		print FD '</b></i><br /><b>Solution. </b>';
+		print FD nl2br issues_get_answers $path;
+		print FD '<br /><font size="2">';
+		print FD nl2br issues_get_info $path;
+		print FD '</font><br /><hr />';
+
+	}
+	closedir(DIR);
+	
+	html_footer *FD;
+	close(FD);
+}
+
+sub make_kernels_page {
+	local *FD;
+	my $kernel;
+	print ">> Making kernels page\n";
+	my %kernels = _get_genpatches_kernels();
+	
+	open(FD, '> '.$output_path.'/kernels.htm');
+	html_header *FD;
+	print FD '<h1>Available Kernels</h1>';
+	print FD '<table class="kernels">';
+	print FD '<tr><th>Kernel</th><th>Version</th><th>Genpatches</th></tr>';
+	foreach $kernel (values %kernels) {
+		print FD '<tr>';
+		print FD '<td>'.$kernel->{'pkg'}.'</td>';
+		print FD '<td>'.$kernel->{'ver'}.'</td>';
+		
+		print FD '<td><a href="patches-'.$kernel->{'gprev'}.'.htm">'.$kernel->{'gprev'}.'</a> '.$kernel->{'wanted'}.'</td>';
+		print FD '</tr>';
+	}
+	print FD '</table>';
+	html_footer *FD;
+	close(FD);
+
+}
+
+sub make_release_pages {
+	my ($cmd, @out, @patches, @patchpages, $patch);
+	local *DIR;
+	print ">> Making release pages\n";
+	$cmd = 'svn ls '.$subversion_root.'/tags';
+	@out = `$cmd`;
+
+	foreach (@out) {
+		chomp;
+		chop;
+		next if $_ !~ /^2\.6\./;
+		if (!release_is_generated($_)) {
+			print ">> Generatating release pages for $_\n";
+			@patches = _get_patch_list($_);
+			generate_patchlist($_, @patches);
+			generate_info($_, @patches);
+		}
+	}
+	
+	make_releases_index();
+	opendir(DIR, $webscript_path.'/generated');
+	@patchpages = grep { /-patches\.htm$/ } readdir DIR;
+	closedir(DIR);
+	
+	foreach $patch (@patchpages) {
+		$patch =~ m/^(.*)-patches\.htm$/;
+		copy($webscript_path.'/generated/'.$patch, $webscript_path.'/output/patches-'.$1.'.htm');
+	}
+}
+
+sub make_releases_index {
+	my (%kernels, $info, @infopages, $kernel);
+	local (*DIR, *FILE, *INDEX);
+	opendir(DIR, $webscript_path.'/generated');
+	@infopages = grep { /-info\.htm$/ } readdir(DIR);
+	foreach $info (@infopages) {
+		$info =~ m/^(\d\.\d\.\d+)-\d+-info\.htm$/;
+		$kernels{$1} = 1;
+	}
+
+	open(INDEX, '> '.$webscript_path.'/output/releases.htm');
+	html_header(INDEX, 'genpatches Releases');
+	print INDEX '<h1>genpatches Releases</h1>';
+	
+	foreach $kernel (sort keys %kernels) {
+		print INDEX '<p><a href="releases-'.$kernel.'.htm">genpatches releases for Linux '.$kernel.'</a></p>';
+		open(FILE, '> '.$webscript_path.'/output/releases-'.$kernel.'.htm');
+		html_header(FILE, "$kernel Releases");
+		print FILE '<h1>'.$kernel.' Releases</h1>';
+		foreach (grep { /^$kernel-/ } @infopages) {
+			include_generated(FILE, $_);
+		}
+		html_footer(FILE);
+		close(FILE);
+	}
+
+	html_footer(INDEX);
+	close (INDEX);
+	closedir(DIR);
+}
+
+sub generate_patchlist {
+	my ($tag, @patches) = @_;
+	local *PATCHLIST;
+	my $patch;
+
+	open(PATCHLIST, '> '.$webscript_path.'/generated/'.$tag.'-patches.htm');
+	html_header(PATCHLIST, "$tag Patch List");
+	print PATCHLIST '<h1>'.$tag.' Patch List</h1>';
+	print PATCHLIST '<p>Patches 0000-2999 are available in ';
+	print PATCHLIST '<a href="tarballs/genpatches-'.$tag.'.base.tar.bz2">genpatches-'.$tag.'.base.tar.bz2</a>';
+	print PATCHLIST '<br />Patches 3000+ are available in ';
+	print PATCHLIST '<a href="tarballs/genpatches-'.$tag.'.extras.tar.bz2">genpatches-'.$tag.'.extras.tar.bz2</a></p>';
+	print PATCHLIST '<table id="patchlist">';
+	print PATCHLIST '<tr><th>Patch</th><th>From</th><th>Description</th></tr>';
+	foreach $patch (@patches) {
+		print PATCHLIST '<tr>';
+		print PATCHLIST '<td class="patch">'.$patch->{'patch'}.'</td>';
+		print PATCHLIST '<td class="from">'.html_urlify($patch->{'from'}).'</td>';
+		print PATCHLIST '<td class="desc">'.$patch->{'desc'}.'</td>';
+		print PATCHLIST '</tr>';
+	}
+	print PATCHLIST '</table>';
+	html_footer(PATCHLIST);
+	close(PATCHLIST);
+}
+
+sub generate_info {
+	my ($tag, @patches) = @_;
+	my (@commits, $ver, $rel, $have_history, $oldtag, @log_lines);
+	my ($lastrev);
+	local *INFO;
+	$tag =~ m/(2\.6\.\d+)-(\d+)/;
+	$ver = $1;
+	$rel = $2;
+	$have_history = 0;
+
+	# Try and find previous release
+	if ($rel > 1) {
+		$oldtag = $ver.'-'.($rel-1);
+		$cmd = 'svn log -q --stop-on-copy '.$subversion_root.'/tags/'.$oldtag;
+		@log_lines = `$cmd`;
+		$lastrev = 0;
+		foreach (@log_lines) {
+			next if $_ !~ /^r(\d+) \|/;
+			$lastrev = $1;
+			last;
+		}
+	}
+
+	if ($lastrev) {
+		@commits = _parse_log($tag, $lastrev);
+		$have_history = @commits;
+	}
+	
+	open (INFO, '> '.$webscript_path.'/generated/'.$tag.'-info.htm');
+	print INFO '<h2>Release '.$tag.'</h2>';
+	print INFO '<p><a href="patches-'.$tag.'.htm">View entire patch list</a><br />';
+	print INFO 'Split-out patch tarballs: ';
+	print INFO '<a href="tarballs/genpatches-'.$tag.'.base.tar.bz2">base</a>, ';
+	print INFO '<a href="tarballs/genpatches-'.$tag.'.extras.tar.bz2">extras</a></p>';
+	
+	if ($lastrev && $have_history) {
+		print INFO '<h3>Changes since '.$oldtag.'</h3>';
+		foreach $rev (@commits) {
+			next if !$rev->{'rev'};
+			print INFO '<p><strong>Revision '.$rev->{'rev'}.':</strong> ';
+			print INFO $rev->{'logmsg'}.' ('.$rev->{'author'}.')<br />';
+			print INFO '<strong>Added:</strong> '.$_.'<br />' foreach (@{$rev->{'actionA'}});
+			print INFO '<strong>Modified:</strong> '.$_.'<br />' foreach (@{$rev->{'actionM'}});
+			print INFO '<strong>Deleted:</strong> '.$_.'<br />' foreach (@{$rev->{'actionD'}});
+			print INFO '</p>';
+		}
+	}
+	print INFO '<hr />';
+	close (INFO);
+}
+
+sub process_static_content {
+	copy($webscript_path.'/content/style.css', $output_path);
+	copy($webscript_path.'/content/.htaccess', $output_path);
+}
+
